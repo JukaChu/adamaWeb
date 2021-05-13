b5eba0447ae496b5a74c957b15107ed0266bb84a
JukaChu <olexiy.yankevich@gmail.com>
2021-05-13 18:12:14 +0300

    home page thanks page

---
+++ dist/js/scripts.min.js
@@ -0,0 +1,66 @@
+let buttonForm = document.querySelector('.form-container .link.link--green');
+
+function ifButtonForm() {
+    if (!buttonForm) {
+
+    } else {
+        buttonForm.addEventListener('click', (e) => {
+            if (buttonForm.classList.contains('active')) {
+
+            } else {
+                e.preventDefault();
+                buttonForm.classList.add('active');
+            }
+        })
+    }
+};
+ifButtonForm();
+
+
+jQuery(function($) {
+
+    const section = $('.follow-class'),
+        nav = $('.menu'),
+        navHeight = nav.outerHeight(); // получаем высоту навигации
+
+    // поворот экрана
+
+    $(window).on('scroll', function () {
+        const position = $(this).scrollTop();
+
+        section.each(function () {
+            const top = $(this).offset().top - navHeight - 5,
+                bottom = top + $(this).outerHeight();
+
+            if (position >= top && position <= bottom) {
+                nav.find('li').removeClass('active');
+                section.removeClass('active');
+
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').closest('li').addClass('active');
+            }
+        });
+    });
+
+    nav.find('a').on('click', function () {
+        const id = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(id).offset().top - navHeight
+        }, 487);
+
+        return false;
+    });
+
+});
+
+$(".content-right a").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#consult-form").offset().top
+    }, 600);
+});
+$(".down-btn").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#download-block").offset().top
+    }, 400);
+});
