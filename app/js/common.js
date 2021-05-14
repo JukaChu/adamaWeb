f026699f79d1b65909404d54738c0bab2b0bac99
JukaChu <olexiy.yankevich@gmail.com>
2021-05-14 15:07:19 +0300

    home page thanks page

--- app/js/common.js
+++ app/js/common.js
@@ -26,7 +26,7 @@ jQuery(function($) {
     // поворот экрана
 
     $(window).on('scroll', function () {
-        const position = $(this).scrollTop() + 300;
+        const position = $(this).scrollTop() + 200;
 
         section.each(function () {
             const top = $(this).offset().top,
@@ -45,9 +45,12 @@ jQuery(function($) {
     nav.find('a').on('click', function () {
         const id = $(this).attr('href');
 
-        $('html, body').animate({
-            scrollTop: $(id).offset().top - 60
-        }, 487);
+            $('html, body').animate({
+
+                scrollTop: $(id).offset().top - 60
+            }, 487);
+
+
 
         return false;
     });
@@ -59,8 +62,89 @@ $(".content-right a").click(function() {
         scrollTop: $("#consult-form").offset().top
     }, 600);
 });
+$(".mob-only.green-link").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#consult-form").offset().top
+    }, 700);
+});
 $(".down-btn").click(function() {
     $([document.documentElement, document.body]).animate({
-        scrollTop: $("#download-block").offset().top
+        scrollTop: $("#download-block").offset().top + 40
     }, 400);
 });
+
+
+// Получаем нужный элемент
+var elementBtns = document.querySelector('.consult-form');
+
+var Visible = function (target) {
+    if (!elementBtns) {
+
+    } else {
+        var targetPosition = {
+                top: window.pageYOffset + target.getBoundingClientRect().top,
+                left: window.pageXOffset + target.getBoundingClientRect().left,
+                right: window.pageXOffset + target.getBoundingClientRect().right,
+                bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+            },
+            // Получаем позиции окна
+            windowPosition = {
+                top: window.pageYOffset + 80,
+                left: window.pageXOffset,
+                right: window.pageXOffset + document.documentElement.clientWidth,
+                bottom: window.pageYOffset + document.documentElement.clientHeight
+            };
+
+        if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+            targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+            targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+            targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+            // Если элемент полностью видно, то запускаем следующий код
+            document.querySelector('.mob-only.green-link').classList.add('hidden');
+        } else {
+            // if hidden el
+            document.querySelector('.mob-only.green-link').classList.remove('hidden');
+        };
+    }
+    // all of position element
+
+};
+
+// start function after scroll
+window.addEventListener('scroll', function() {
+    Visible (elementBtns);
+});
+
+// start function after load
+Visible (elementBtns);
+
+//open video modal
+
+let videoOpener = document.querySelector('.expr-video a');
+let videoModal = document.querySelector('.modal-window--video');
+let closeModal = document.querySelector('.close-modal');
+
+function ifVideoOpenedOnPage() {
+    if(!videoOpener) {
+
+    } else {
+        videoOpener.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.body.classList.add('no-scroll');
+            videoModal.classList.add('open');
+        });
+
+        closeModal.addEventListener('click', () => {
+            closeModal.closest('.modal-window').classList.remove('open');
+            document.body.classList.remove('no-scroll');
+        });
+        videoModal.addEventListener('click', () => {
+            videoModal.classList.remove('open');
+            document.body.classList.remove('no-scroll');
+        });
+        videoModal.querySelector('.modal-container').addEventListener('click', (e) => {
+            e.stopPropagation();
+        })
+    }
+}
+ifVideoOpenedOnPage();
